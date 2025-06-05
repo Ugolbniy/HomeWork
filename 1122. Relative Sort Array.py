5887d5cc7831fc2ae261640d465e0c8b40bcfb1d
Ugolbniy <160910744+Ugolbniy@users.noreply.github.com>
2025-06-05 12:47:00 +0300

    Update 1122. Relative Sort Array.py

--- 1122. Relative Sort Array.py
+++ 1122. Relative Sort Array.py
@@ -10,9 +10,3 @@ class Solution:
         for num, freq in remaining:
             result.extend([num] * freq)
         return result
-
-if __name__ == "__main__":
-    sol = Solution()
-    arr1 = [2,3,1,3,2,4,6,7,9,2,19]
-    arr2 = [2,1,4,3,9,6]
-    print(sol.relativeSortArray(arr1, arr2))
