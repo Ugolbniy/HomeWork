013bf6b85af5511a8b6fb3848844e1151525d96c
Ugolbniy <160910744+Ugolbniy@users.noreply.github.com>
2025-06-05 12:46:36 +0300

    1636. Sort Array by Increasing Frequency.py

--- 1636. Sort Array by Increasing Frequency.py
+++ 1636. Sort Array by Increasing Frequency.py
@@ -4,8 +4,3 @@ class Solution:
     def frequencySort(self, nums):
         count = Counter(nums)
         return sorted(nums, key=lambda x: (count[x], -x))
-
-if __name__ == "__main__":
-    sol = Solution()
-    nums = [1,1,2,2,2,3]
-    print(sol.frequencySort(nums))
