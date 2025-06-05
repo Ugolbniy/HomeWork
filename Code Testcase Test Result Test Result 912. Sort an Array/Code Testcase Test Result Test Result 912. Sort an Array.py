8caeb89da0164853a9e3f02cc46b4440eeddcec0
Ugolbniy <160910744+Ugolbniy@users.noreply.github.com>
2025-06-05 12:32:16 +0300

    912. Sort an Array.py

---
+++ Code Testcase Test Result Test Result 912. Sort an Array/Code Testcase Test Result Test Result 912. Sort an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums):
+        return sorted(nums)
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [5, 2, 3, 1]
+    print(sol.sortArray(nums))
